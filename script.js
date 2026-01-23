976d3e00b0e54602eb33a8e79aa4c1024405aeaa
tulen.segar.asli <nursyazwani5567@gmail.com>
2026-01-23 16:01:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function orderNow() {
+  alert("Thank you for your interest! Please contact us via WhatsApp to order.");
+}
+
+function openWhatsApp() {
+  window.open("https://wa.me/60192700366", "_blank");
+}
