2d84a77175cf1d54d29f368979def6fa960ec904
tulen.segar.asli <nursyazwani5567@gmail.com>
2026-01-27 14:38:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ function orderNow() {
 }
 
 function openWhatsApp() {
-  window.open("https://wa.me/60192700366", "_blank");
+  window.open(
+    "https://wa.me/60192700366?text=Hi%20%F0%9F%98%8A%20Saya%20berminat%20untuk%20order%20santan.%20Boleh%20bagi%20maklumat%20lanjut%3F",
+    "_blank"
+  );
 }
+
